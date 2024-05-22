f3cd05dec1c478e87fcfa67022a2de7a2d9b2dc6
Serhii Zubkov <serhii.zubkov@litslink.com>
2024-05-22 20:41:03 +0300

    add lesson3.js

---
+++ lesson3.js
@@ -0,0 +1,30 @@
+const person1 = {
+  name: 'John',
+  age: 25,
+}
+
+const person2 = {
+  name: 'Bill',
+  age: 30,
+}
+
+const cat = {
+  name: 'Fluffy',
+  color: 'black'
+}
+
+const city = {
+  name: 'New York',
+  population: 10000000
+}
+
+function sayHi(isHi) {
+  console.log(`Hi, ${this.name}`);
+}
+
+sayHi.call(
+  {
+    name: 'Fluffy',
+    color: 'black'
+  }
+);
