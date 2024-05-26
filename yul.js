ece2fa82e863b3ff9c2e71e57c71210a22cf37f6
Yuliia Svitlychna <yuliia.svitlychna@gmail.com>
2024-05-26 03:29:48 +0300

    Created personas, their ages and pets.

--- yul.js
+++ yul.js
@@ -1,3 +1,5 @@
+/*
+
 var sum = 0;
 var i=1;
 const interval = setInterval(function(){
@@ -14,3 +16,39 @@ const interval = setInterval(function(){
     clearInterval(interval);
    }
 },1000);
+
+*/
+
+const person1 {
+    name: 'Agata',
+    age: 25,
+    pet: 'snake'
+}
+
+const person2 {
+    name: 'Bill',
+    age: 30,
+    pet: 'dog'
+}
+
+const person3 {
+    name: 'Emilia',
+    age: 21,
+    pet: 'cat'
+}
+
+const person4 {
+    name: 'Smith',
+    age: 23,
+    pet: 'fish'
+}
+
+const person5 {
+    name: 'Johnson',
+    age: 32,
+    pet: 'bird'
+}
+
+function sayHi (isHi){
+console.log('Hi, ${this.name}')
+}
