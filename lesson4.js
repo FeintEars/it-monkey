3688ed1c4039c681eab5521f821f446781f0d9e5
Serhii Zubkov <serhii.zubkov@litslink.com>
2024-05-26 21:01:15 +0300

    lesson4.js

---
+++ lesson4.js
@@ -0,0 +1,18 @@
+
+
+/*
+function Person (name, age) {
+  this.name = name;
+  this.age = age;
+}
+*/
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const person1 = new Person('John', 25);
+const person2 = new Person('Bill', 30);
