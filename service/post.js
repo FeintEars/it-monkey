a95a5137ea745660fe68808382f18b1cae5335ce
Serhii Zubkov <serhii.zubkov@litslink.com>
2024-07-19 20:45:23 +0300

    change author to authorId for database level

--- service/post.js
+++ service/post.js
@@ -4,21 +4,27 @@ import {
   databaseUpdatePost,
   databaseDeletePost,
 } from "../database/post.js";
+import { databaseReadUser } from "../database/user.js";
 
 // C - Create
 async function serviceCreatePost(title, body, author) {
-  return databaseCreatePost(title, body, author);
+  const post = await databaseCreatePost(title, body, author.id);
+  post.author = await databaseReadUser(post.authorId);
+  return post;
 }
 
 // R - Read
 async function serviceReadPost(id) {
-  return databaseReadPost(id);
+  const post = await databaseReadPost(id);
+  post.author = await databaseReadUser(post.authorId);
+  return post;
 }
 
 // U - Update
 async function serviceUpdatePost(id, title, body) {
   const post = await databaseReadPost(id);
-  return databaseUpdatePost(id, title, body, post.author);
+  post.author = await databaseReadUser(post.authorId);
+  return databaseUpdatePost(id, title, body, post.author.id);
 }
 
 // D - Delete
