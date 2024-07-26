51eaf73991e2721e4a4a7f55a90032e0119df39b
Yevhen Omelianenko <evhenomelyanenko@gmail.com>
2024-07-27 00:36:34 +0300

    brainstorm

--- service/post.js
+++ service/post.js
@@ -5,20 +5,28 @@ import {
   databaseDeletePost,
 } from "../database/post.js";
 
+import { databaseReadUser } from "../database/user.js";
+
 // C - Create
 async function serviceCreatePost(title, body, author) {
-  return databaseCreatePost(title, body, author);
+  const post = await databaseCreatePost(title, body, author.id);
+  post.author = await databaseReadUser(author.id)
+  return post;
 }
 
 // R - Read
 async function serviceReadPost(id) {
-  return databaseReadPost(id);
+  const post = await databaseReadPost(id);
+  post.author = await databaseReadUser(post.authorId)
+  return post;
 }
 
 // U - Update
 async function serviceUpdatePost(id, title, body) {
-  const post = await databaseReadPost(id);
-  return databaseUpdatePost(id, title, body, post.author);
+  let post = await databaseReadPost(id);
+  post = await databaseUpdatePost(id,title,body,post.authorId);
+  post.author = await databaseReadUser(post.authorId);
+  return post;
 }
 
 // D - Delete
