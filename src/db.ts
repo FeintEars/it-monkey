f33a6908f661abe5f7eec45cc7ab3992a5ddbe02
Yuliia Svitlychna <yuliia.svitlychna@gmail.com>
2024-12-13 23:41:12 +0200

    Add .env

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,18 @@
+import dotenv from "dotenv"
 import { DataSource } from "typeorm";
 import { User } from "./entities/user";
 import { Post } from "./entities/post";
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "admin",
-  password: "admin",
-  database: "itmonkey",
+  host: process.env.DB_HOST,
+  port: parseInt (process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: [User, Post],
   subscribers: [],
   migrations: [],
